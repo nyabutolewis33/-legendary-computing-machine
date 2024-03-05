e9219fbadf9239c370289cdaa78d149da543c1e1
nyabutolewis33 <nyabutolewis33@gmail.com>
2024-03-05 15:00:46 +0300

    second commit

--- Final_project/routes/web.php
+++ Final_project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HouseHuntingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/home',[HouseHuntingController::class,'index']);
+
